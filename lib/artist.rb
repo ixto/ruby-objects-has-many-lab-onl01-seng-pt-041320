e44b8285a4178c7d19a743445f18fe543efaca36
Joey <joey.alzamora@gmail.com>
2020-05-18 20:43:48 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,49 @@
+require 'pry' 
+
 class Artist 
 
-  attr_accessor :name 
-  
+  attr_accessor :name
+  @@song_count = 0 
+  # attr_reader :songs 
+ 
   def initialize(name)
     @name = name 
-    @songs = []
+    @songs = [] 
   end 
   
-  def songs
-    @songs 
+  # def songs
+  #   @songs
+  # end 
+  
+  def add_song(song)
+    @songs << song 
+    song.artist = self 
+    @@song_count += 1 
   end 
-
+  
+  def add_song_by_name(name)
+    s = Song.new(name)
+    self.add_song(s)
+    s 
+    @@song_count += 1 
+  end 
+  
+  def songs 
+     artist_songs = Song.all.select do |song| 
+        song.artist == self
+      end
+      artist_songs
+    end
+  
+  def song_count 
+    self.song.size 
+  end 
+  
+  def self.song_count
+    @@song_count 
+  end 
+  
+  
 end 
 
 # class Artist
@@ -28,4 +61,41 @@ end
 #   def songs
 #     @songs
 #   end
+# end
+
+
+# class Artist
+#     attr_accessor :name
+  
+#     @@song_count = 0
+  
+#     def initialize(name)
+#       @name = name
+#       @songs = []
+#     end
+  
+#     def add_song(song)
+#       @songs << song
+#       song.artist = self
+#       @@song_count +=1
+#     end
+
+#     def add_song_by_name(name)
+#       song = Song.new(name)
+#       @songs << song
+#       song.artist = self
+#       @@song_count +=1
+#     end
+  
+#     def songs
+#       artist_songs = Song.all.select do |song| 
+#         song.artist == self
+#       end
+#       artist_songs
+#     end
+  
+#     def self.song_count
+#       @@song_count
+#   end
+
 # end
--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,24 @@
+# class Song 
+
+#   attr_accessor :title, :artist 
+#   @@all = []
+#   def initialize(title)
+#     @title = title 
+#     @@all << self 
+#   end 
+  
+# end 
+
 class Song 
-
-
-end 
+  @@all = []
+  attr_accessor :name, :songs, :artist  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
